07c09b9b56954ad301b4f08614f9837bc7ca0270
charon <karon9@users.noreply.github.com>
2021-07-26 03:10:06 +0900

    minor change

--- split_data.py
+++ split_data.py
@@ -2,17 +2,17 @@ import pandas as pd
 
 query_data = pd.read_csv('test_query_data.csv')
 split_number = int(len(query_data) / 8)
-train_query_data = query_data.iloc[: split_number * 5]
-val_query_data = query_data.iloc[split_number * 5:split_number * 7]
-test_query_data = query_data.iloc[split_number * 7:]
+train_query_data = query_data.iloc[: split_number * 4]
+val_query_data = query_data.iloc[split_number * 4:split_number * 6]
+test_query_data = query_data.iloc[split_number * 6:]
 first_split_number = train_query_data['horse_number'].sum()
 second_split_number = first_split_number + val_query_data['horse_number'].sum()
 
 
 def split_data_base_on_query(origin_data):
-    train_data = origin_data.iloc[:first_split_number + 1]
-    val_data = origin_data.iloc[first_split_number + 1: second_split_number + 1]
-    test_data = origin_data.iloc[second_split_number + 1:]
+    train_data = origin_data.iloc[:first_split_number]
+    val_data = origin_data.iloc[first_split_number: second_split_number]
+    test_data = origin_data.iloc[second_split_number:]
     return train_data, val_data, test_data
 
 
