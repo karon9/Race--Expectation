245c6d2eb579715e5fe2551fac907d8d61321a39
charon <karon9@users.noreply.github.com>
2021-07-27 00:38:19 +0900

    Being able to predict

--- learn.py
+++ learn.py
@@ -2,9 +2,10 @@ import lightgbm as lgb
 import os
 import pandas as pd
 from split_data import split_data
+from drop_dataset import unnamed_race_id_drop
 
 
-def learn(train_data, train_target, train_query):
+def learn(Train_data, Val_data, Train_target, Val_target, Train_query, Val_query):
     # lightGBMのパラメータ設定
     max_position = 20
     lgbm_params = {
@@ -16,22 +17,30 @@ def learn(train_data, train_target, train_query):
         'boosting_type': 'gbdt',
         'random_state': 777,
     }
+    Train_query = pd.Series(Train_query['horse_number'])
+    Val_query = pd.Series(Val_query['horse_number'])
     # 学習
-    # train = lgb.Dataset(train_data, train_target, group=train_query)
-    # valid = lgb.Dataset(val_data, val_target, reference=lgtrain, group=val_query)
-    # model = lgb.train(
-    #     lgbm_params,
-    #     lgtrain,
-    #     num_boost_round=100,
-    #     valid_sets=valid,
-    #     valid_names=['train', 'valid'],
-    #     early_stopping_rounds=20,
-    #     verbose_eval=5
-    # )
+    lgb_train = lgb.Dataset(Train_data, Train_target, group=Train_query)
+    lgb_valid = lgb.Dataset(Val_data, Val_target, group=Val_query, reference=lgb_train)
+    model = lgb.train(
+        params=lgbm_params,
+        train_set=lgb_train,
+        num_boost_round=100,
+        valid_sets=lgb_valid,
+        valid_names=['train', 'valid'],
+        early_stopping_rounds=20,
+        verbose_eval=5
+    )
 
 
 if __name__ == '__main__':
+    # race_idにsortする。
     data = pd.read_csv('test_horse_data.csv').sort_values(['race_id', 'rank'])
     target_data = data['rank']
-    train_data, val_data, test_data, train_target_data, val_target_data, test_target_data, train_query_data, val_query_data, test_query_data = split_data(
+    data = data.drop('rank', axis=1)
+    train_data, val_data, test_data, train_target, val_target, test_target, train_query, val_query, test_query = split_data(
         data, target_data)
+    train_data = unnamed_race_id_drop(train_data)
+    val_data = unnamed_race_id_drop(val_data)
+    test_data = unnamed_race_id_drop(test_data)
+    learn(train_data, val_data, train_target, val_target, train_query, val_query)
